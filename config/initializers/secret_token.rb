96d87f859bebd29dcb42948b57d8b5c4e6d581c6
aardvarkk <clarksonian@gmail.com>
2014-03-07 14:52:12 -0500

    Blank Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FtPoll::Application.config.secret_token = '163eaf5f13299e5248706376c6aa5ff7e869aca6fb2dda04e47c8410fea68beb35d33f8f39420980cb77dc4252a434279c311aec582f0d4918ef421b539aaa49'

784a55aeb318de359f797ad2a6a7720652a993cc
aardvarkk <clarksonian@gmail.com>
2014-03-12 10:50:30 -0400

    Reversing page order so newest is at the top. This way the default route shows you newest posts first. This meant removing the highlighting of edits, but they still should be reasonably obvious.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = PostEntry.paginate(:page => params[:page]).order(:post_id, :updated_at)
+    @posts = PostEntry.paginate(:page => params[:page]).order('post_id DESC').order('updated_at DESC')
   end
 end
